f19d7fab0861e64400c39fa6f09fa6b7c9389d1e
Masaaki Yana <nativeyana@gmail.com>
2019-12-04 18:19:34 -0800

    Mockup: home/about pages

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django import forms 
+
+class IncidentReport(forms.Form):
+    reporter = forms.CharField(max_length=20)
+    email = forms.CharField(max_length=20)
+    phone = forms.CharField(max_length=20)
+
+    date_incident = forms.CharField(max_length=20)
+    date_reported = forms.CharField(max_length=20)
+    date_update = forms.CharField(max_length=20)
+    description = forms.CharField(max_length=20)
+
 
 # Two example views. Change or delete as necessary.
 def home(request):
@@ -15,3 +27,9 @@ def about(request):
 
     return render(request, 'pages/about.html', context)
 
+
+def add_new(request):
+    context = {
+    }
+
+    return render(request, 'pages/about.html', context)
