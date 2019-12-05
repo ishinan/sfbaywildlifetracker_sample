2be9a2761e37b19ef4d08e36f783c3f888db1b2a
Masaaki Yana <nativeyana@gmail.com>
2019-12-04 23:29:23 -0800

    base.py: removed STATIC_ROOT

--- map_geodjango/config/base.py
+++ map_geodjango/config/base.py
@@ -114,11 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/app/core/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'app/core/static/')
 
-# STATICFILES_DIRS = (
-#     (os.path.join(BASE_DIR, 'static')),
-# )
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR, '../apps/core/static')),
+)
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
