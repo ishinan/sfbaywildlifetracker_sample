f5e23d9cec25cf5cb2c615318e6d2802dc09eaa1
Masaaki Yana <nativeyana@gmail.com>
2019-12-04 22:18:09 -0800

    views.py and add_report.html: Added page for Report a new incident

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django import forms 
+
 
 class IncidentReport(forms.Form):
     reporter = forms.CharField(max_length=20)
     email = forms.CharField(max_length=20)
     phone = forms.CharField(max_length=20)
 
+    location_address = forms.CharField(max_length=120)
     date_incident = forms.CharField(max_length=20)
     date_reported = forms.CharField(max_length=20)
     date_update = forms.CharField(max_length=20)
@@ -29,7 +32,15 @@ def about(request):
 
 
 def add_new(request):
+    if request.method == 'POST':
+        form = IncidentReport(request.POST)
+        if form.is_valid():
+            return HttpResponse("<h1>Thank you for the report!</h1>")
+    else:
+        form = IncidentReport()
+
     context = {
+        'form': form,
     }
 
-    return render(request, 'pages/about.html', context)
+    return render(request, 'pages/add_report.html', context)
